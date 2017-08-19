e1c6bb557420fc31ddfbe4212e24000d4e1319e9
tushtu <this@that.com>
2017-08-18 19:51:46 -0700

    Added comment stuff and renamed

--- srt2subtitleshtml.py
+++ srt2subtitleshtml.py
@@ -7,7 +7,7 @@ from yattag import Doc
 doc, tag, text = Doc().tagtext()
 
 srt = 'Bharatham.srt'
-html = 'index.html'
+html = 'subtitles.html'
 
 subs = pysrt.open(srt)
 
@@ -23,6 +23,8 @@ with tag('html'):
                 doc.stag('br')
                 with tag('audio',controls="",preload="none"):
                     doc.stag('source',src='site/'+subtitle_id+'.ogg',type="audio/ogg")
+                    with tag('a',href="#",onclick="parent.window.frames['comments'].location = 'comments/" + subtitle_id + ".html'"):
+                        text('Comments')
                 doc.stag('hr')
 
 with open(html,'w') as f:
