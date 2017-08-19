3b4ccd8c69c54cc56d0b308e3ced1e34d3989748
tushtu <this@that.com>
2017-08-18 20:43:58 -0700

    Fixed comments placement

--- srt2subtitleshtml.py
+++ srt2subtitleshtml.py
@@ -5,27 +5,30 @@ import pysrt
 # For generating the HTML file
 from yattag import Doc
 doc, tag, text = Doc().tagtext()
-# For mkdir
-import os
 
 srt = 'Bharatham.srt'
+html = 'subtitles.html'
+site = 'site/'
 comments = 'comments/'
 
-os.mkdir(comments)
-
 subs = pysrt.open(srt)
-# We do not want the first subtitle
-for idx,sub in enumerate(subs[1:]):
-  subtitle_id = str(2+idx)
-  doc.asis('<!DOCTYPE html>')
-  with tag('html'):
-      with tag('body'):
-          with tag('script'):
-              text("var idcomments_acct = '7720157ebbf514bb31abf7430d18bb2c';")
-              text("var idcomments_post_id;")
-              text("var idcomments_post_url;")
-      doc.stag('span',id="IDCommentsPostTitle",style="display:none"): 
-      doc.stag('script',type="text/javascript",src="https://www.intensedebate.com/js/genericCommentWrapperV2.js"):
 
-  with open(comments + subtitle_id + '.html','w') as f:
-      f.write(doc.getvalue())
+doc.asis('<!DOCTYPE html>')
+with tag('html'):
+    with tag('body'):
+        doc.stag('hr')
+        # We do not want the first subtitle
+        for idx,sub in enumerate(subs[1:]):
+            subtitle_id = str(2+idx)
+            with tag('p'):
+                text(subtitle_id+'. '+sub.text)
+                doc.stag('br')
+                with tag('audio',controls="",preload="none"):
+                    doc.stag('source',src=site+subtitle_id+'.ogg',type="audio/ogg")
+                doc.stag('br')
+                with tag('a',href="#",onclick="parent.window.frames['comments'].location = '"+comments+subtitle_id + ".html'"):
+                    text('Comments')
+                doc.stag('hr')
+
+with open(html,'w') as f:
+    f.write(doc.getvalue())
